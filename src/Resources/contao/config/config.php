30557905cfb64493ae9adda849b53dd2c3586b35
janborg <jan-github@luenborg.eu>
2021-12-23 18:59:15 +0100

    config Model, Table and Controller

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,6 +1,14 @@
 <?php
 
+use Janborg\H4aGamestats\Model\H4aPlayerscoresModel;
+
 /**
  * Add backend table to calendar
  */
 $GLOBALS['BE_MOD']['content']['calendar']['tables'][] = 'tl_h4a_playerscores';
+$GLOBALS['BE_MOD']['content']['calendar']['lookup_scores'] = ['Janborg\H4aGamestats\Backend\LookupScoresController', 'lookupScores'];
+
+/**
+ * models
+ */
+$GLOBALS['TL_MODELS']['tl_h4a_playerscores'] = H4aPlayerscoresModel::class;
