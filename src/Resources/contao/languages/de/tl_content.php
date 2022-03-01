1681a081e2ccece974304afbca18750c3f678e27
janborg <jan-github@luenborg.eu>
2022-03-01 19:15:53 +0100

    add Lables for cte

---
+++ src/Resources/contao/languages/de/tl_content.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+
+$GLOBALS['TL_LANG']['CTE']['h4a_gamescore']['0'] = 'Spielstatistik Team';
+$GLOBALS['TL_LANG']['CTE']['h4a_gamescore']['1'] = 'Erzeugt die Spielstatistik für das angegebene Team';
+
+$GLOBALS['TL_LANG']['CTE']['h4a_seasonscore']['0'] = 'Saisonstatistik Team';
+$GLOBALS['TL_LANG']['CTE']['h4a_seasonscore']['1'] = 'Erzeugt die Saisonstatistik für das angegebene Team';
