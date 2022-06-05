06d43ea52602362c7ecde83fd903397683d94896
Janborg <jan-github@luenborg.eu>
2022-06-05 09:18:16 +0200

    php8 fix

--- src/Resources/contao/dca/tl_h4a_timeline.php
+++ src/Resources/contao/dca/tl_h4a_timeline.php
@@ -65,7 +65,7 @@ $GLOBALS['TL_DCA']['tl_h4a_timeline'] = [
                 'label' => &$GLOBALS['TL_LANG']['tl_h4a_timeline']['delete'],
                 'href' => 'act=delete',
                 'icon' => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
             'show' => [
                 'label' => &$GLOBALS['TL_LANG']['tl_h4a_timeline']['show'],
