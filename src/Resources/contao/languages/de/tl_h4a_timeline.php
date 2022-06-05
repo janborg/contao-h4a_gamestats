5a2292ff8062f56bfef28ac4c0cf2f3d41a620f5
Janborg <jan-github@luenborg.eu>
2022-06-05 12:33:43 +0200

    add translations

---
+++ src/Resources/contao/languages/de/tl_h4a_timeline.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of contao-h4a_gamestats.
+ *
+ * (c) Jan Lünborg
+ *
+ * @license MIT
+ */
+
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['lookup_timeline'] = 'Spielverlauf abrufen';
+
+
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['matchtime'][0] = 'Spielzeit';
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['matchtime'][1] = 'Spielzeit der Aktion';
+
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['currentscore'][0] = 'Aktueller Spielstand';
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['currentscore'][1] = 'Aktueller Spielstand nach der Aktion';
+
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['action_team'][0] = 'Team';
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['action_team'][1] = 'Team der aktuellen Aktion';
+
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['action_player'][0] = 'Spieler';
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['action_player'][1] = 'Spieler der aktuellen Aktion';
+
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['action_player_number'][0] = 'Spielernummer';
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['action_player_number'][1] = 'Nummer des Spielers';
+
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['action_type'][0] = 'Aktionstyp';
+$GLOBALS['TL_LANG']['tl_h4a_timeline']['action_type'][1] = 'Typ der aktuellen Aktion, bspw. "Tor", "Gelbe Karte", "7m"';
