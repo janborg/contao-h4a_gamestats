99b2437803140c1706031241b24ac522b8ed9444
Janborg <jan-github@luenborg.eu>
2023-01-15 14:24:06 +0100

    add customTempleate Options

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -18,9 +18,9 @@ use Janborg\H4aGamestats\Controller\ContentElement\H4aTimelineElement;
  * palettes
  */
 
-$GLOBALS['TL_DCA']['tl_content']['palettes'][H4aGameScoreElement::TYPE] = '{type_legend},type,headline;{h4a_legend},team_calendar,h4a_season,h4a_event_id;{expert_legend:hide},cssID';
-$GLOBALS['TL_DCA']['tl_content']['palettes'][H4aSeasonScoreElement::TYPE] = '{type_legend},type,headline;{h4a_legend},team_calendar,h4a_season,my_team_name;{expert_legend:hide},cssID';
-$GLOBALS['TL_DCA']['tl_content']['palettes'][H4aTimelineElement::TYPE] = '{type_legend},type,headline;{h4a_legend},team_calendar,h4a_season,h4a_event_id;{expert_legend:hide},cssID';
+$GLOBALS['TL_DCA']['tl_content']['palettes'][H4aGameScoreElement::TYPE] = '{type_legend},type,headline;{h4a_legend},team_calendar,h4a_season,h4a_event_id;{template_legend:hide},customTpl;{expert_legend:hide},cssID';
+$GLOBALS['TL_DCA']['tl_content']['palettes'][H4aSeasonScoreElement::TYPE] = '{type_legend},type,headline;{h4a_legend},team_calendar,h4a_season,my_team_name;{template_legend:hide},customTpl;{expert_legend:hide},cssID';
+$GLOBALS['TL_DCA']['tl_content']['palettes'][H4aTimelineElement::TYPE] = '{type_legend},type,headline;{h4a_legend},team_calendar,h4a_season,h4a_event_id;{template_legend:hide},customTpl;{expert_legend:hide},cssID';
 
 /*
  * Fields
