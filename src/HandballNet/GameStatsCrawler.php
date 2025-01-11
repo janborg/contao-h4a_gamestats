373e82ca3cacb9a3be0499d2f6f652d8a1b439f0
Janborg <jan-github@luenborg.eu>
2025-01-11 18:43:39 +0100

    initial commit for GamestatsCrawler

---
+++ src/HandballNet/GameStatsCrawler.php
@@ -0,0 +1,229 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of contao-h4a_gamestats.
+ *
+ * (c) Jan Lünborg
+ *
+ * @license MIT
+ */
+
+namespace Janborg\H4aGamestats\HandballNet;
+
+use InvalidArgumentException;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpClient\HttpClient;
+
+class GameStatsCrawler
+{
+    private string $baseUrl = 'https://www.handball.net';
+
+    private string $verbandName;
+
+    private string $verbandShortname;
+
+    private string $classID;
+
+    private string $classShortName;
+
+    private string $gGameID;
+
+    private array $homeLineup;
+
+    private array $guestLineup;
+
+    private array $timeline;
+
+    private array $matchInfo;
+
+    private Crawler $crawler;
+
+
+    public function setClassID(string $classID): void
+    {
+        $this->classID = $classID;
+    }
+
+    public function setClassShortName(string $classShortName): void
+    {
+        $this->classShortName = urlencode($classShortName);
+        $this->classShortName = strtolower($this->classShortName);
+    }
+
+    public function setgGameID(string $gGameID): void
+    {
+        $this->gGameID = $gGameID;
+    }
+
+    public function setVerbandName(string $verbandName): void
+    {
+        $this->verbandName = urlencode($verbandName);
+    }
+
+    public function setVerbandShortname(string $verbandShortname): void
+    {
+        $this->verbandShortname = urlencode($verbandShortname);
+    }
+
+    public function getHomeTeam(): string
+    {
+        return $this->matchInfo["homeTeam"];
+    }
+
+    public function getGuestTeam(): string
+    {
+        return $this->matchInfo["guestTeam"];
+    }
+
+    public function getMatchResult(): string
+    {
+        return $this->matchInfo["homeTeamResult"].' : '.$this->matchInfo["guestTeamResult"];
+    }
+
+    public function getHomeLineup(): array
+    {
+        return $this->homeLineup;
+    }
+
+    public function getGuestLineup(): array
+    {
+        return $this->guestLineup;
+    }
+
+    public function getTimeline(): array
+    {
+        return $this->timeline;
+    }
+
+    private function getGameUrl(): string
+    {
+        return $this->baseUrl . '/ligen/handball4all.' . $this->verbandName . '.' . $this->classShortName . '_' . $this->verbandShortname . '/spielplan/spieltage/handball4all.' . $this->verbandName . '.' . $this->classID . '/spiele/handball4all.' . $this->verbandName . '.'. $this->gGameID;
+    }
+
+    private function getCrawler(): void
+    {
+        $url = $this->getGameUrl();
+
+        $httpClient = HttpClient::create();
+
+        $response = $httpClient->request('GET', $url);
+
+        $html = $response->getContent();
+
+        $this->crawler = new Crawler($html);
+    }
+
+    private function crawlMatch(): void
+    {
+        $matchTable = $this->crawler->filterXPath('//div[@id="tickaroo-liveblog"]//table')->first();
+
+        $matchInfo = [];
+        try {
+            $matchTable->filter('td')->each(function (Crawler $node, $i) use (&$matchInfo) {
+                switch ($i) {
+                    case 0:
+                        $matchInfo["homeTeam"] = $node->text();    
+                        break;
+                    case 1:
+                        $matchInfo["homeTeamResult"] = $node->text();
+                        break;
+                    case 2:
+                        $matchInfo["guestTeam"] = $node->text();    
+                        break;
+                    case 3:
+                        $matchInfo["guestTeamResult"] = $node->text();
+                        break;
+                }
+            });
+            $this->matchInfo = $matchInfo;
+        } catch (InvalidArgumentException $e) {
+            $this->matchInfo = $matchInfo;
+        }
+    }
+
+    private function crawlLineups(): void
+    {
+        // find all tables inside div with id 'aufstellung'
+        $allTables = $this->crawler->filterXPath('//div[@id="aufstellung"]//table/tbody');
+
+        // loop through all tables and return the node values as array
+        $arrTables = $allTables->each(function (Crawler $tableCrawler) {
+            $lineupArray = $tableCrawler->filter('tr')->each(function (Crawler $rowCrawler)  {
+                $rowArray = $rowCrawler->filter('td')->each(function (Crawler $cellCrawler)  {
+                    return $cellCrawler->text();
+                });
+                return $rowArray;
+            });
+            return $lineupArray;
+        });
+
+        $this->homeLineup = $arrTables[0];
+        $this->guestLineup = $arrTables[1];
+    }
+    
+    private function crawlTimeline(): void
+    {
+        // find the ul element inside the div with id 'tickaroo-liveblog'
+        $ulTimeline = $this->crawler->filterXPath('//div[@id="tickaroo-liveblog"]//ul');
+
+        $timelineEvents = [];
+
+        try {
+            $ulTimeline->filter('li')->each(function (Crawler $liCrawler, $i) use (&$timelineEvents) {
+                $timelineEvents[$i]["timestamp"] = $liCrawler->filter('div')->eq(0)->filter('span')->text();
+                
+                $timelineEvents[$i]["standing"] = $liCrawler->filter('div > p')->eq(0)->text();
+
+                $timelineEvents[$i]["eventText"] = $liCrawler->filter('div >p')->eq(1)->text();
+            });
+        } catch (InvalidArgumentException $e) {
+            $this->timeline = array_reverse($timelineEvents, false);
+        }
+    }
+
+    /**
+     * creates a Crawler and crawls all the game stats for a game from handball.net. Relevant inputs must be set upfront.
+     *
+     * @return void
+     */
+    public function getAllGameStats(): void
+    {
+        $this->getCrawler();
+        
+        $this->crawlMatch();
+
+        $this->crawlLineups();
+
+        $this->crawlTimeline();
+    }
+
+    /**
+     * Creates a Crawler and crawls the lineup for both teams of a game from handball.net. Relevant inputs must be set upfront.
+     *
+     * @return void
+     */
+    public function getGameLineups(): void
+    {
+        $this->getCrawler();
+        
+        $this->crawlMatch();
+
+        $this->crawlLineups();
+    }
+
+    /**
+     * Creates a Crawler and crawls the timeline of a game from handball.net. Relevant inputs must be set upfront.
+     *
+     * @return void
+     */
+    public function getGameTimeline(): void
+    {
+        $this->getCrawler();
+        
+        $this->crawlMatch();
+
+        $this->crawlTimeline();
+    }
+}
