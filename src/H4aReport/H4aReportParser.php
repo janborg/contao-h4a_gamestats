2f15da31bf3f340952664c8beb403cb393054fda
janborg <jan+github@luenborg.eu>
2025-12-18 17:04:50 +0100

    chore: fix phpstan missingType.iterableValue

--- src/H4aReport/H4aReportParser.php
+++ src/H4aReport/H4aReportParser.php
@@ -20,17 +20,17 @@ use Symfony\Component\HttpClient\HttpClient;
 /**
  * Class H4aReportParser.
  *
- * @property array        $home_team
- * @property array        $guest_team
- * @property string       $heim_name
- * @property string       $gast_name
- * @property array        $timeline
- * @property string       $reportUrl
- * @property string       $jsonReport
- * @property array<mixed> $arrReport
- * @property string       $gameNo
- * @property array<mixed> $zuschauer
- * @property array<mixed> $schiedsrichter
+ * @property array<string, mixed> $home_team
+ * @property array<string, mixed> $guest_team
+ * @property string               $heim_name
+ * @property string               $gast_name
+ * @property array<string, mixed> $timeline
+ * @property string               $reportUrl
+ * @property string               $jsonReport
+ * @property array<mixed>         $arrReport
+ * @property string               $gameNo
+ * @property array<mixed>         $zuschauer
+ * @property array<mixed>         $schiedsrichter
  */
 class H4aReportParser
 {
